06022465cc00f46f5a2c904c4ce4b4c7475aa9fc
Anderson Guedes <anderson.guedes@outlook.com.br>
2024-09-15 09:58:05 -0300

    Add Comment

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinShark.Interfaces;
+using FinShark.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinShark.Controllers
+{
+    [Route("api/comment")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentRepository _commentRepo;
+
+        public CommentController(ICommentRepository commentRepo)
+        {
+            _commentRepo = commentRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var comments = await _commentRepo.GetAllAsync();
+            var commentsDto = comments.Select(s => s.ToCommentDto());
+
+            return Ok(commentsDto);
+        }
+    }
+}
---
+++ Mappers/CommentMappers.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinShark.Dtos.Comment;
+using FinShark.Models;
+
+namespace FinShark.Mappers
+{
+    public static class CommentMappers
+    {
+        public static CommentDto ToCommentDto(this Comment commentModel)
+        {
+            return new CommentDto
+            {
+                Id = commentModel.Id,
+                Title = commentModel.Title,
+                Content = commentModel.Content,
+                CreatedOn = commentModel.CreatedOn,
+                StockId = commentModel.StockId,
+            };
+        }
+    }
+}

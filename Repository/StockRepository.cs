4a7f19b6748afee8581d443cdc8b577d5d94f3a4
Anderson Guedes <anderson.guedes@outlook.com.br>
2024-09-16 19:39:50 -0300

    Add Paginação

--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -63,7 +63,9 @@ namespace FinShark.Repository
                 }
             }
 
-            return await stocks.ToListAsync();
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
 
         }
 

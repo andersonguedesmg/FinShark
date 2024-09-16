1e526315c059bb21d3036044511f5ee43c96c1bd
Anderson Guedes <anderson.guedes@outlook.com.br>
2024-09-16 19:31:58 -0300

    Add Sorting

--- Helpers/QueryObject.cs
+++ Helpers/QueryObject.cs
@@ -10,5 +10,9 @@ namespace FinShark.Helpers
         public string? Symbol { get; set; } = null;
 
         public string? CompanyName { get; set; } = null;
+
+        public string? SortBy { get; set; } = null;
+
+        public bool IsDecsending { get; set; } = false;
     }
 }
